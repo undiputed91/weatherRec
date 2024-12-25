e3c190f74b41c8904dff9ace8b934016e332caea
undiputed91 <shin2896@naver.com>
2024-12-25 20:34:51 +0900

    git init

---
+++ src/main/java/com/example/weatherrec/WeatherRecApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherRecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weatherrec/WeatherRecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
